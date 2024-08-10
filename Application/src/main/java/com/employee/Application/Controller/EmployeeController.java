095fc1420472f9212865b7dbe7d2a943a316b39e
saikrishna <msaikrishna2603@gmail.com>
2024-08-09 21:53:33 -0500

    Changes in controller class

--- Application/src/main/java/com/employee/Application/Controller/EmployeeController.java
+++ Application/src/main/java/com/employee/Application/Controller/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/employees")
 @AllArgsConstructor
